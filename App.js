167cb675cba4802a5fd77f3238a9f3f0c7cf7c05
Nisha Gode <nishagode@gmail.com>
2019-03-12 18:04:18 -0400

    added icon and style

--- App.js
+++ App.js
@@ -1,33 +1,77 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, SafeAreaView } from "react-native";
 import styled from "styled-components";
 import Card from "./components/Card";
+import { Icon } from "expo";
+import { NotificationIcon } from "./components/Icons";
+import Logo from "./components/Logo";
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <TitleBar>
-          <Avatar source={require("./assets/avatar.jpg")} />
-          <Title>Welcome back,</Title>
-          <Name>Nisha</Name>
-        </TitleBar>
-        <Subtitle>Continue Learning</Subtitle>
-        <Card />
+        <SafeAreaView>
+          <ScrollView style={{ height: "100%" }}>
+            <TitleBar>
+              <Avatar source={require("./assets/avatar.jpg")} />
+              <Title>Welcome back,</Title>
+              <Name>Nisha</Name>
+              <NotificationIcon
+                style={{ position: "absolute", right: 20, top: 5 }}
+              />
+            </TitleBar>
+            <ScrollView
+              style={{
+                flexDirection: "row",
+                padding: 20,
+                paddingLeft: 12,
+                paddingTop: 30
+              }}
+              horizontal={true}
+            >
+              <Logo
+                image={require("./assets/logo-framerx.png")}
+                text="Framer X"
+              />
+              <Logo image={require("./assets/logo-figma.png")} text="Figma" />
+            </ScrollView>
+            <Subtitle>Continue Learning</Subtitle>
+            <ScrollView
+              horizontal={true}
+              style={{ paddingBottom: 30 }}
+              showsHorizontalScrollIndicator={false}
+            >
+              <Card
+                title="Styled Components"
+                image={require("./assets/background2.jpg")}
+                caption="React Native"
+                logo={require("./assets/logo-react.png")}
+                subtitle="5 of 12 sections"
+              />
+              <Card
+                title="Styled Components 2"
+                image={require("./assets/background1.jpg")}
+                caption="React Native"
+                logo={require("./assets/logo-react.png")}
+                subtitle="5 of 12 sections"
+              />
+            </ScrollView>
+          </ScrollView>
+        </SafeAreaView>
       </Container>
     );
   }
 }
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #f0f3f5;
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 10px;
+  text-transform: uppercase;
 `;
-const TitleBar = styled.View`
-  width: 100%;
-  margin-top: 50px;
-  padding-left: 80px;
-`;
+
 const Avatar = styled.Image`
   width: 44px;
   height: 44px;
@@ -35,22 +79,29 @@ const Avatar = styled.Image`
   border-radius: 22px;
   margin-left: 20px;
   position: absolute;
+  top: 0;
+  left: 0;
 `;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
 const Title = styled.Text`
   font-size: 16px;
   color: #b8bece;
   font-weight: 500;
 `;
+
 const Name = styled.Text`
   font-size: 20px;
   color: #3c4560;
   font-weight: bold;
 `;
-const Subtitle = styled.Text`
-  color: #b8bece;
-  font-weight: 600;
-  font-size: 15px;
-  margin-left: 20px;
+
+const TitleBar = styled.View`
+  width: 100%;
   margin-top: 50px;
-  text-transform: uppercase;
+  padding-left: 80px;
 `;
